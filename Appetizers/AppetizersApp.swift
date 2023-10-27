873efe45fffc398e8ef4b64d7ceda87163528aac
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2023-10-27 12:42:12 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Alejandro Sierro Galán on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
